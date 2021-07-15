e852b70def11625492288d354c4cf09ec25f5c6e
Denise Gratia Aruan <denisearuan19@student.uns.ac.id>
2021-07-15 11:35:53 +0700

    first commit

---
+++ database/seeders/pegawaiSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+
+class pegawaiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Pegawai::class)->make();
+    }
+}
